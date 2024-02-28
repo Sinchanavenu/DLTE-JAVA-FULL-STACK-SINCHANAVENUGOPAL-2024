60194ea39923c7abf4df8244a0602f641062f2e3
Sinchanavenu <sinchanavenugopal@gmail.com>
2024-02-28 15:32:15 +0530

    Uploaded task-005

--- DLTE-Java/DLTE-Java-005/src/main/java/basic/service/ExceptionHandling/Execute.java
+++ DLTE-Java/DLTE-Java-005/src/main/java/basic/service/ExceptionHandling/Execute.java
@@ -1,9 +1,14 @@
 package basic.service.ExceptionHandling;
 
+import java.util.ResourceBundle;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Execute {
     public static void main(String[] args) {
+        ResourceBundle resourceBundle=ResourceBundle.getBundle("application");
+        Logger logger=Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
         Gpay gpay[]= new Gpay[10];
         gpay[0]=new Gpay(3456132584L,654240L,"Sahana","sahana",6100);
         Scanner scanner=new Scanner(System.in);
@@ -13,8 +18,12 @@ public class Execute {
             int choice=scanner.nextInt();
             switch(choice){
                 case 1:
-                    gpay[0].payBill("Ninadha",600L,"Recharge");
-                    break;
+                    try {
+                        gpay[0].payBill("Ninadha", 600L, "Recharge");
+                        break;
+                    } catch(MyBankException exception){
+                        logger.log(Level.WARNING,exception.toString());
+                    }
                 case 2:
                     System.exit(0);
             }
