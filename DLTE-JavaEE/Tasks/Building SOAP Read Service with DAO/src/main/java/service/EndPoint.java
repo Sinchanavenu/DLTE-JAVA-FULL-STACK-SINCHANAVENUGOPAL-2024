fc1c3cb418cb7cdcd8ea0320d9d0c3fb70bc7305
Sinchanavenu <sinchanavenugopal@gmail.com>
2024-03-19 10:13:54 +0530

    Uploaded tasks

---
+++ DLTE-JavaEE/Tasks/Building SOAP Read Service with DAO/src/main/java/service/EndPoint.java
@@ -0,0 +1,13 @@
+package service;
+
+
+import javax.xml.ws.Endpoint;
+
+public class EndPoint {
+    private static String url="http://localhost:1000/transactions";
+    public static void main(String[] args) {
+        TransactionByUsername transactionByUsername=new TransactionByUsername();
+        System.out.println("Webservice hosted @ "+url);
+        Endpoint.publish(url,transactionByUsername);
+    }
+}
