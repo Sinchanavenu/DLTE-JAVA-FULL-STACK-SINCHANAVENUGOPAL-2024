ac1aeab3827fef37f310c980dfaf4d098cb92e24
Sinchanavenu <sinchanavenugopal@gmail.com>
2024-03-25 15:44:05 +0530

    uploaded spring task

--- DLTE-Spring/Tasks/Spring with JPA - Account/src/main/java/spring/jpa/demo/ServletInitializer.java
+++ DLTE-Spring/Tasks/Spring with JPA - Account/src/main/java/spring/jpa/demo/ServletInitializer.java
@@ -2,11 +2,10 @@ package spring.jpa.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import spring.jpa.demo.DemoApplication;
 
 public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringJpaAppilcation.class);
+        return application.sources(SpringApplicationBuilder.class);
     }
 }
